b09b9bbd70f4cfc04829f04c8f0bcfb22b63ca5f
Jay Nayak <jnayak12@bcit.ca>
2022-11-28 13:25:15 -0800

    Implement websocket in React.

---
+++ App.js
@@ -0,0 +1,45 @@
+import React, { Component, Suspense } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      client: null,
+    };
+    this.connect = this.connect.bind(this);
+  }
+  componentDidMount() {
+    this.interval = setInterval(this.connect, 1000);
+  }
+  componentWillUnmount() {
+    if (this.ws) this.ws.close();
+    if (this.interval) clearInterval(this.interval);
+  }
+  connect = () => {
+    if (this.ws === undefined || (this.ws && this.ws.readyState === 3)) {
+      this.ws = new WebSocket(`ws://127.0.0.1:8000/ws/`);
+    }
+    this.ws.onopen = () => {
+      console.log("Websocket Client Connected");
+      this.setState({ client: this.ws });
+    };
+    this.ws.onmessage = (message) => {
+      const dataFromServer = JSON.parse(message.data);
+      if (dataFromServer) {
+        console.log("got Reply!", dataFromServer);
+      }
+    };
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <ToastContainer />
+        <Suspense fallback={null}>
+          <main></main>
+        </Suspense>
+      </React.Fragment>
+    );
+  }
+}
+export default App;
